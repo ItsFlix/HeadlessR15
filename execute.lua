9fb68ffbb7576a31f8b1c819ea5120688eac6ff2
ItsFlix <66955402+ItsFlix@users.noreply.github.com>
2026-07-12 03:59:35 -0300

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,3 +1,3 @@
 local a = Instance.new('LocalScript',game:GetService("StarterPlayer").StarterCharacterScripts)
 a.Name = ' Animate '
-a.Source = loadstring(game:HttpGet('https://pastebin.com/raw/qWUx23C2'))()
+a.Source = loadstring(game:HttpGet('https://raw.githubusercontent.com/ItsFlix/HeadlessR15/refs/heads/main/LocalScript.lua'))()

